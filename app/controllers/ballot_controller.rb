fb93248195af33ed02464cb15ddf644a3b4a0b5d
Kyle <kyle.bailey@ymail.com>
2014-11-17 13:25:51 -0600

    Fixed association bug and added each loop to race controller

--- app/controllers/ballot_controller.rb
+++ app/controllers/ballot_controller.rb
@@ -35,66 +35,66 @@ class BallotController < ApplicationController
 
 		#president/vice
 		@race = @ballot.races.create(category:"President and Vice President", race_type:"Choose One", title:"President and Vice President", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Gordon Bearce, Nathan Maclean", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Vernon Stanley Albury, Richard Rigby", party: "DEM", race_id: @race.id)
-		@race.candidates.create(name: "Janette Froman, Chris Aponte", party: "LIB", race_id: @race.id)
+		@race.candidate.create(name: "Gordon Bearce, Nathan Maclean", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Vernon Stanley Albury, Richard Rigby", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Janette Froman, Chris Aponte", party: "LIB", race_id: @race.id)
 
 		#senator
 		@race = @ballot.races.create(category:"Congressional", race_type:"Choose One", title:"United States Senator", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Cecile Cadieux", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Fern Brzezinski", party: "DEM", race_id: @race.id)
-		@race.candidates.create(name: "Corey Dery", party: "IND", race_id: @race.id)
+		@race.candidate.create(name: "Cecile Cadieux", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Fern Brzezinski", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Corey Dery", party: "IND", race_id: @race.id)
 
 		#representative
 		@race = @ballot.races.create(category:"Congressional", race_type:"Choose One", title:"Representative in Congress", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Pedro Brouse", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Robert Mettler", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Pedro Brouse", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Robert Mettler", party: "DEM", race_id: @race.id)
 
 		#governor
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Governor", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Glen Travis Lozier", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Rick Stickles", party: "DEM", race_id: @race.id)
-		@race.candidates.create(name: "Maurice Humble", party: "IND", race_id: @race.id)
+		@race.candidate.create(name: "Glen Travis Lozier", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Rick Stickles", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Maurice Humble", party: "IND", race_id: @race.id)
 
 		#governor
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Lieutenent Governor", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Shane Terrio", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Cassie Principe", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Shane Terrio", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Cassie Principe", party: "DEM", race_id: @race.id)
 
 		#attorney general
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Attorney General", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Tim Speight", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Rick Organ", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Tim Speight", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Rick Organ", party: "DEM", race_id: @race.id)
 
 		#comptroller
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Comptroller of Public Accounts", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Therese Gustin", party: "IND", race_id: @race.id)
-		@race.candidates.create(name: "Greg Converse", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Therese Gustin", party: "IND", race_id: @race.id)
+		@race.candidate.create(name: "Greg Converse", party: "DEM", race_id: @race.id)
 
 		#land office
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Commissioner of General Land Office", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Sam Saddler", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Elise Ellzey", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Sam Saddler", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Elise Ellzey", party: "DEM", race_id: @race.id)
 
 		#agriculture
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Commissioner of Agriculture", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Polly Rylander", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Roberto Aron", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Polly Rylander", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Roberto Aron", party: "DEM", race_id: @race.id)
 
 		#railroad
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"Railroad Commissioner", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Jillian Balas", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Zachary Minick", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Jillian Balas", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Zachary Minick", party: "DEM", race_id: @race.id)
 
 		#state senator
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"State Senator", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Ricardo Nigro", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Wesley Steven Millette", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Ricardo Nigro", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Wesley Steven Millette", party: "DEM", race_id: @race.id)
 
 		#state representative
 		@race = @ballot.races.create(category:"State", race_type:"Choose One", title:"State Representative", ballot_id: @ballot.id)
-		@race.candidates.create(name: "Petra Bencomo", party: "REP", race_id: @race.id)
-		@race.candidates.create(name: "Susanne Rael", party: "DEM", race_id: @race.id)
+		@race.candidate.create(name: "Petra Bencomo", party: "REP", race_id: @race.id)
+		@race.candidate.create(name: "Susanne Rael", party: "DEM", race_id: @race.id)
 
 
 		redirect_to ballot_url(@ballot)
