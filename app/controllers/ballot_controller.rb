94a9da3b74614830dfe0da3e90d9c0fd793f3c7e
Kyle <kyle.bailey@ymail.com>
2014-11-18 16:51:28 -0600

    Finished adding in all the races on the ballot

--- app/controllers/ballot_controller.rb
+++ app/controllers/ballot_controller.rb
@@ -149,6 +149,31 @@ class BallotController < ApplicationController
 		@race.candidate.create(name: "Yes", race_id: @race.id)
 		@race.candidate.create(name: "No", race_id: @race.id)
 
+		#prop 2
+		@race = @ballot.races.create(category:"Proposition", race_type:"Y/N", title:"Proposition 2", ballot_id: @ballot.id, description:"Shall the Charter of Harris County beamended to authorize the City Council to review and approve certain intergovernmental agreements and revenue contracts entered into by the City; to permit the City Council to establish its meeting schedule by ordinance; to clarify the circumstances in which the City Council may act by ordinance or resolution; to permit the City Council to adopt by ordinance procedures for the formation and administration of special assessment districts; to permit excused absences of council members for reasons other than sickness; and to make other conforming amendments related thereto in order to eliminate redundant or obsolete provisions of the charter?")
+		@race.candidate.create(name: "Yes", race_id: @race.id)
+		@race.candidate.create(name: "No", race_id: @race.id)
+
+		#prop 3
+		@race = @ballot.races.create(category:"Proposition", race_type:"Y/N", title:"Proposition 3", ballot_id: @ballot.id, description:"Shall there be an amendment to the Texas constitution concerning recoveryof damages relating to construction of real property improvements, and, in connection therewith, prohibiting laws that limit or impair a property owner’s right to recover damages caused by a failure to construct an improvement in a good and workmanlike manner; defining “good and workmanlike manner” to include construction that is suitable for its intended purposes; and permitting exceptions for laws that limit punitive damages, afford governmental immunity, or impose time limits of specified minimum lengths on filing lawsuits?")
+		@race.candidate.create(name: "Yes", race_id: @race.id)
+		@race.candidate.create(name: "No", race_id: @race.id)
+
+		#prop 4
+		@race = @ballot.races.create(category:"Proposition", race_type:"Y/N", title:"Proposition 4", ballot_id: @ballot.id, description:"Shall there be an amendment to theTexas revised statutes concerning renewable energy standards for large providers of retail electric service, and, in connection therewith, defining eligible renewable energy resources to include solar, wind, geothermal, small hydroelectricity, and hydrogen fuel cells; requiring that a percentage of retail electricity sales be derived from renewable sources, beginning with 3% in the year 2013 and increasing to 10% by 2021; requiring utilities to offer consumers a rebate of $2.00 per watt and other incentives for solar electric generation; providing incentives for utilities to invest in renewable energy resources that provide net economic benefits to customers; limiting the retail rate impact of renewable energy resources to 50 cents per month for residential customers; requiring public utilities commission rules to establish major aspects of the measure; prohibiting utilities from using condemnation or eminent domain to acquire land for generating facilities used to meet the standards; requiring utilities with requirements contracts to address shortfalls from the standards; and specifying election procedures by which the customers of a utility may opt out of the requirements of the amendment?")
+		@race.candidate.create(name: "Yes", race_id: @race.id)
+		@race.candidate.create(name: "No", race_id: @race.id)
+
+		#prop 5
+		@race = @ballot.races.create(category:"Proposition", race_type:"Y/N", title:"Proposition 5", ballot_id: @ballot.id, description:"Shall there be an amendment to theTexas constitution concerning election day voter registration, and, in connection therewith, allowing an eligible citizen to register and vote on any day that a vote may be cast in any election beginning on January 1, 2013; specifying election day voter registration locations; specifying that an eligible citizen who registers to vote on election day shall register in person and present a current and valid Texas driver’s license or state identification card or other approved documentation; and directing the Texas general assembly, in implementing election day voter registration, to adopt necessary protections against election fraud?")
+		@race.candidate.create(name: "Yes", race_id: @race.id)
+		@race.candidate.create(name: "No", race_id: @race.id)
+
+		#prop 6
+		@race = @ballot.races.create(category:"Proposition", race_type:"Y/N", title:"Proposition 6", ballot_id: @ballot.id, description:"Shall the Charter of Harris Countyconcerning the powers of the City Council be amended in regard to the sale of city-owned property, to require Council approval for the sale of personal property valued at $500,000 or more, and to clarify language requiring Council approval of any sale of real property?")
+		@race.candidate.create(name: "Yes", race_id: @race.id)
+		@race.candidate.create(name: "No", race_id: @race.id)
+
 
 		redirect_to "/ballot/#{@ballot.id}/instructions"
 	end
